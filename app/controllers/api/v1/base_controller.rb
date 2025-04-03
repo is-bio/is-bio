6b5fa678183a654b270de615305319a509e91e23
Lane Zhang <zjloveztt@gmail.com>
2025-04-03 08:12:27 +0800

    Inherited from `ActionController::API` for `api/v1` controllers.

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,4 @@
-class Api::V1::BaseController < ActionController::Base
-  skip_forgery_protection
-
+class Api::V1::BaseController < ActionController::API
   rescue_from ActionController::RoutingError, ActiveRecord::RecordNotFound do |exception|
     render json: { error: exception.message }, status: :not_found
   end
