d80053130df8d6b6ad15025d6fb08bb42330e22d
Lane Zhang <zjloveztt@gmail.com>
2025-04-13 16:56:20 +0800

    Added `es-ES.yml`, `ja.yml`, `pt-BR.yml`, `ru.yml`.

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,5 +1,12 @@
 # Permitted locales available for the application
-I18n.available_locales = [ :en, :"zh-CN" ]
+I18n.available_locales = [
+  :en,
+  # :"zh-CN",
+  # :"es-ES",
+  # :ja,
+  # :ru,
+  # :"pt-BR",
+]
 
 # Set default locale to something other than :en
 # I18n.default_locale = :"zh-CN"
