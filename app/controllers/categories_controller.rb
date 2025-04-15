0bed3c90998918d1ce7a889fffb9a7cbf9e0b8f4
Lane Zhang <zjloveztt@gmail.com>
2025-04-15 16:37:22 +0800

    Renamed `category` to `@category` and use it in pages.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,28 +11,28 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    category = Category.find_by_name(params.expect(:name))
+    @category = Category.find_by_name(params.expect(:name))
 
-    if category.nil?
+    if @category.nil?
       raise_404
     end
 
     translated_posts
 
-    category_ids = category.descendant_ids << category.id
+    category_ids = @category.descendant_ids << @category.id
     @posts = @posts.includes(:category).where(category_id: category_ids).order(published_at: :desc)
   end
 
   def drafts_show
-    category = Category.find_by_name_from_drafts(params.expect(:name))
+    @category = Category.find_by_name_from_drafts(params.expect(:name))
 
-    if category.nil?
+    if @category.nil?
       raise_404
     end
 
     translated_posts
 
-    category_ids = category.descendant_ids << category.id
+    category_ids = @category.descendant_ids << @category.id
     @posts = @posts.includes(:category).where(category_id: category_ids).order(published_at: :desc)
 
     render :show
