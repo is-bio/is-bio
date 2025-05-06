386be5b9a4f663a4b29725bb3eb358704d810684
Zhang Jian <zjloveztt@gmail.com>
2025-05-06 13:17:41 +0800

    Removed the anchor in heading of post/show page, but allowed user to click the heading and navigate to that heading section.

--- app/javascript/pages/post_show.js
+++ app/javascript/pages/post_show.js
@@ -7,32 +7,36 @@ function pageLoaded() {
 }
 
 function createHeadingAnchors() {
-  const firstHeadingAnchor = document.querySelector(".blog-post-body h1 a, .blog-post-body h2 a, .blog-post-body h3 a, .blog-post-body h4 a, .blog-post-body h5 a, .blog-post-body h6 a");
+  const firstHeadingAnchor = document.querySelector(".blog-post-body h1 a, .blog-post-body h2 a, .blog-post-body h3 a, .blog-post-body h4 a");
 
   // Prevent from creating `heading anchors` multiple times because in some conditions (e.g.: on post show page, click home page, then and click home show page again), `'turbo:load'` event was triggered more than once.
   if (firstHeadingAnchor != null) {
     return;
   }
 
-  const headingList = document.querySelectorAll('.blog-post-body h1, .blog-post-body h2, .blog-post-body h3, .blog-post-body h4, .blog-post-body h5, .blog-post-body h6');
+  const headingList = document.querySelectorAll('.blog-post-body h1, .blog-post-body h2, .blog-post-body h3, .blog-post-body h4');
 
   headingList.forEach((heading) => {
     const headingId = encodeURIComponent(heading.textContent.split(' ')[0].toLowerCase());
     heading.id = headingId;
+    heading.style.cursor = 'pointer';
 
-    const anchor = document.createElement('a');
-    anchor.href = `#${headingId}`;
-    anchor.classList.add('d-none');
-    anchor.classList.add('text-muted');
-    anchor.text = ' #';
-    heading.insertAdjacentElement('beforeend', anchor);
+    heading.addEventListener('click', function (e) {
+      e.preventDefault();
 
-    heading.addEventListener('mouseover', togglePositionAnchor);
-    heading.addEventListener('mouseout', togglePositionAnchor);
+      window.location.hash = headingId;
+
+      heading.scrollIntoView({ behavior: 'smooth' });
+    });
   });
 
-  if (location.href.includes('#')) {
-    window.location.href = location.href; // We have to revisit to jump because the heading id(s) are just created and so the page cannot be positioned to that id.
+  if (location.hash) {
+    const targetElement = document.querySelector(location.hash);
+    if (targetElement) {
+      setTimeout(() => {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      }, 100);
+    }
   }
 }
 
